ba1f95ad7ff6f246cbd984fdb931101d7e1e0b56
Mathias Chevez <solo@Mathiass-MacBook-Air.local>
2022-06-23 21:23:35 -0600

    attempt 2

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div>
         <h1>FOOTER</h1>
       </div>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3005.4082866198787!2d-112.07914468429252!3d41.12561222014451!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87531ae6af5e7fef%3A0x69207904a80241c8!2s2618%20W%20800%20N%2C%20Clinton%2C%20UT%2084015!5e0!3m2!1sen!2sus!4v1655220995454!5m2!1sen!2sus" width="600" height="300" allowfullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3005.4082866198787!2d-112.07914468429252!3d41.12561222014451!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87531ae6af5e7fef%3A0x69207904a80241c8!2s2618%20W%20800%20N%2C%20Clinton%2C%20UT%2084015!5e0!3m2!1sen!2sus!4v1655220995454!5m2!1sen!2sus" width="600" height="300" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
   )
 }
