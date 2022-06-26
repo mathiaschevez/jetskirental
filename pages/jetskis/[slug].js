8ba1427903e828e7190588cb1514abaf8b08dfbe
Mathias Chevez <solo@Mathiass-MacBook-Air.local>
2022-06-25 18:53:52 -0600

    adding quantity functionality

--- pages/jetskis/[slug].js
+++ pages/jetskis/[slug].js
@@ -21,7 +21,6 @@ const JetskiDetail = ({ jetski, jetskis }) => {
   const maxDate = new Date(today)
   maxDate.setMonth(maxDate.getMonth() + 2)
 
-
   useEffect(() => {
     if(!daysSelected.length > 0) {
       setRemoving(-1)
@@ -55,91 +54,98 @@ const JetskiDetail = ({ jetski, jetskis }) => {
   }
 
   const handleDisableDates = ({ date }) => {
-    console.log(date)
+    const daysInDb = jetski?.bookings?.filter((day) => {
+      return dayjs(day).format('YYYY-MM-DD') === dayjs(date).format('YYYY-MM-DD')
+    })
+    console.log(daysInDb)
 
-    return (jetski?.bookings?.includes(dayjs(date).format('YYYY-MM-DD'))) || (dayjs(date).format('ddd') === 'Sun')
+    return (daysInDb?.length === jetski?.quantity) || (dayjs(date).format('ddd') === 'Sun')
   }
 
   return (
-    <div className='flex flex-col md:flex-row md:items-start lg:w-2/3 m-auto gap-6 justify-around items-center py-12 lg:py-16'>
-      <div className='flex flex-col px-3 items-center md:items-start'>
-        <h1 className='font-bold text-2xl'>Booking</h1>
-        <h1 className='font-semibold mb-6'>Select days to book</h1>
-        <div className='flex justify-center items-center sm:w-2/3 md:w-full lg:w-full xl:w-full'>
-          <Calendar 
-            className='rounded-xl' 
-            value={currentDate} 
-            onChange={setCurrentDate}
-            next2Label={null}
-            prev2Label={null}
-            tileDisabled={({ date }) => handleDisableDates({ date})}
-            minDate={today}
-            maxDate={maxDate}
-            prevLabel={(<IoArrowBack size={22}/>)}
-            nextLabel={(<IoArrowForward size={22}/>)}
-          />
-        </div>
+    <div className='py-12 lg:py-16 lg:w-2/3 m-auto'>
+      <div className='px-3 mb-6'>
+        <h1 className='text-2xl font-semibold'>Booking</h1>
+        <h1>Select days to book</h1>
       </div>
-      <div>
-        <div className='px-3'>
-          <img src={urlFor(jetski.image[0])} className='rounded m-auto'/>
-        </div>
-        <div className='px-3 flex justify-between items-center mt-3'>
-          <h1 className='text-black font-bold text-2xl'>{jetski?.name}</h1>
-          <div className='flex'>
-            <h1 className='font-semibold'>$</h1>
-            <h1 className='font-bold text-2xl'>{jetski?.price}</h1>
-            <h1 className='text-xs self-end'>/day</h1>
+      <div className='flex flex-col md:flex-row md:items-start gap-6 justify-around items-center '>
+        <div className='flex flex-col px-3 items-center md:items-start'>
+          <div className='flex justify-center items-center sm:w-2/3 md:w-full lg:w-full xl:w-full'>
+            <Calendar 
+              className='rounded-xl' 
+              value={currentDate} 
+              onChange={setCurrentDate}
+              next2Label={null}
+              prev2Label={null}
+              tileDisabled={({ date }) => handleDisableDates({ date})}
+              minDate={today}
+              maxDate={maxDate}
+              prevLabel={(<IoArrowBack size={22}/>)}
+              nextLabel={(<IoArrowForward size={22}/>)}
+            />
           </div>
         </div>
-        {daysSelected[0] && (
-          <div className='px-3 mt-6'>
-            <h1 className='font-semibold mb-3'>Days selected</h1>
-            <div className='grid grid-cols-2 gap-3'>
-              {daysSelected?.map((day, i) => (
-                <div key={i} className={`${removing === i && 'border-red-600'} flex gap-3 bg-[#ebebeb] px-2 py-2 justify-between rounded border border-slate-400`}>
-                  <h1 className='px-2'>{dayjs(day).format('MMM DD')}</h1>
-                  <button 
-                    onMouseEnter={() => setRemoving(i)} 
-                    onMouseLeave={() => setRemoving(-1)}
-                    onClick={() => handleRemoveDay(dayjs(day).format('MMM DD'))}
-                    className='text-red-600 px-2 rounded' 
-                  >
-                    <AiOutlineCloseCircle size={24}/>
-                  </button>
-                </div>
-              ))}
+        <div className='w-full'>
+          <div className='px-3'>
+            <img src={urlFor(jetski.image[0])} className='rounded m-auto'/>
+          </div>
+          <div className='px-3 flex justify-between items-center mt-3'>
+            <h1 className='text-black font-bold text-2xl'>{jetski?.name}</h1>
+            <div className='flex'>
+              <h1 className='font-semibold'>$</h1>
+              <h1 className='font-bold text-2xl'>{jetski?.price}</h1>
+              <h1 className='text-xs self-center ml-1'>/day</h1>
             </div>
           </div>
-        )}
-        <div className=''>
-          {daysSelected[0] ? (
-            <div className='flex gap-3 px-3 mt-12'>
-              <button onClick={() => handleAdd()} className='transition ease-in-out w-1/2 border-2 border-[#00A7C3] rounded font-bold text-[#00A7C3] hover:bg-[#00A7C3] hover:text-white'>Add to cart</button>
-              <button onClick={() => handleBuyNow()} className='transition ease-in-out w-1/2 border-2 border-[#00A7C3] rounded font-bold text-[#00A7C3] hover:bg-[#00A7C3] hover:text-white py-3'>Book Now</button>
-            </div>
-          ) : (
-            <div className='flex gap-3 px-3 mt-12'>
-              <h1 onClick={() => toast.error('Please add days to book.')} className='text-center py-3 w-1/2 border-2 border-slate-500 rounded font-bold text-slate-500'>Add to cart</h1>
-              <h1 onClick={() => toast.error('Please add days to book.')} className='text-center py-3 w-1/2 border-2 border-slate-500 rounded font-bold text-slate-500'>Book Now</h1>
-            </div>
-          )}
-        </div>
-        <div className='mt-6 px-3 mb-6'>
-          {!viewOthers ? (
-            <button onClick={() => setViewOthers(true)} className='transition ease-in-out text-black border-2 rounded border-black w-full py-2 hover:bg-black hover:text-white font-bold'>View Catalog</button>
-          ) : (
-            <button onClick={() => setViewOthers(false)} className='transition ease-in-out text-black border-2 rounded border-black w-full py-2 hover:bg-black hover:text-white font-bold'>Close Catolog</button>
-          )}
-          {viewOthers &&
-            <div className='mt-3'>
-              <div className='grid grid-cols-1 gap-1'>
-                {jetskis.map((jetski) => (
-                  <Jetski key={jetski._id} jetski={jetski} />
+          {daysSelected[0] && (
+            <div className='px-3 mt-6'>
+              <h1 className='font-semibold mb-3'>Days selected</h1>
+              <div className='grid grid-cols-2 gap-3'>
+                {daysSelected?.map((day, i) => (
+                  <div key={i} className={`${removing === i && 'border-red-600'} flex gap-3 bg-[#ebebeb] px-2 py-2 justify-between rounded border border-slate-400`}>
+                    <h1 className='px-2'>{dayjs(day).format('MMM DD')}</h1>
+                    <button 
+                      onMouseEnter={() => setRemoving(i)} 
+                      onMouseLeave={() => setRemoving(-1)}
+                      onClick={() => handleRemoveDay(dayjs(day).format('MMM DD'))}
+                      className='text-red-600 px-2 rounded' 
+                    >
+                      <AiOutlineCloseCircle size={24}/>
+                    </button>
+                  </div>
                 ))}
               </div>
             </div>
-          }
+          )}
+          <div className=''>
+            {daysSelected[0] ? (
+              <div className='flex gap-3 px-3 mt-12'>
+                <button onClick={() => handleAdd()} className='transition ease-in-out w-1/2 border-2 border-[#00A7C3] rounded font-bold text-[#00A7C3] hover:bg-[#00A7C3] hover:text-white'>Add to cart</button>
+                <button onClick={() => handleBuyNow()} className='transition ease-in-out w-1/2 border-2 border-[#00A7C3] rounded font-bold text-[#00A7C3] hover:bg-[#00A7C3] hover:text-white py-3'>Book Now</button>
+              </div>
+            ) : (
+              <div className='flex gap-3 px-3 mt-12'>
+                <h1 onClick={() => toast.error('Please add days to book.')} className='text-center py-3 w-1/2 border-2 border-slate-500 rounded font-bold text-slate-500'>Add to cart</h1>
+                <h1 onClick={() => toast.error('Please add days to book.')} className='text-center py-3 w-1/2 border-2 border-slate-500 rounded font-bold text-slate-500'>Book Now</h1>
+              </div>
+            )}
+          </div>
+          <div className='mt-6 px-3 mb-6'>
+            {!viewOthers ? (
+              <button onClick={() => setViewOthers(true)} className='transition ease-in-out text-black border-2 rounded border-black w-full py-2 hover:bg-black hover:text-white font-bold'>View Catalog</button>
+            ) : (
+              <button onClick={() => setViewOthers(false)} className='transition ease-in-out text-black border-2 rounded border-black w-full py-2 hover:bg-black hover:text-white font-bold'>Close Catolog</button>
+            )}
+            {viewOthers &&
+              <div className='mt-3'>
+                <div className='grid grid-cols-1 gap-1'>
+                  {jetskis.map((jetski) => (
+                    <Jetski key={jetski._id} jetski={jetski} />
+                  ))}
+                </div>
+              </div>
+            }
+          </div>
         </div>
       </div>
     </div>
